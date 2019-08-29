7f9f8e9fda576c765a9d612865cb160190a0eb78
akagrawat <akagrawat@gmail.com>
2019-08-29 19:13:47 +0530

    Update UI & snake functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,79 @@
 let resolution = 20;
 let food;
 
-class Snake{
-    constructor(){
-        this.snakeBox = []; 
+class Snake {
+    constructor() {
+        this.snakeBox = [];
         this.snakeBox[0] = createVector(0, 0);
         this.snakeLen = 0;
         this.xdir = 1;
         this.ydir = 0;
     }
 
-    setDir(x,y){
+    setDir(x, y) {
         this.xdir = x;
         this.ydir = y;
     }
 
-    update()
-    {
-        let head = this.snakeBox[this.snakeBox.length-1].copy();
-        console.log(this.snakeBox.length);
+    update() {
+        let head = this.snakeBox[this.snakeBox.length - 1].copy();
         this.snakeBox.shift();
         head.x += this.xdir;
         head.y += this.ydir;
         this.snakeBox.push(head);
+
         // this.snakeBox[0].x += this.xdir;
         // this.snakeBox[0].y += this.ydir;
+
     }
 
-    grow(){
-        let head = this.snakeBox[this.snakeBox.length-1].copy();
+    grow(pos) {
         this.snakeLen++;
-        this.snakeBox.push(head);
+        this.snakeBox.push(pos);
     }
 
-    show(){
-        for(let i = 0; i<this.snakeBox.length ; i++){
-
-        
-        fill(255);
-        noStroke(); 
-        rect(this.snakeBox[i].x,this.snakeBox[i].y,1,1);
-
+    show() {
+        for (let i = 0; i < this.snakeBox.length; i++) {
+            fill(0, 255, 0);
+            noStroke();
+            rect(this.snakeBox[i].x, this.snakeBox[i].y, 1, 1);
         }
-        
+
     }
 
-    foodLocation(){
+    foodLocation() {
         let x = floor(random(w));
         let y = floor(random(h));
-        food = createVector(x,y);
+        food = createVector(x, y);
     }
 
-   
-    eat(pos){
-        let x = this.snakeBox[this.snakeBox.length-1].x;
-        let y = this.snakeBox[this.snakeBox.length-1].y;
 
-        if(x === pos.x && y === pos.y){
-            this.grow();
+    eat(pos) {
+        let x = this.snakeBox[this.snakeBox.length - 1].x;
+        let y = this.snakeBox[this.snakeBox.length - 1].y;
+
+        if (x === pos.x && y === pos.y) {
+            this.grow(pos);
             return true;
         }
-            return false;
+        return false;
     }
- 
 
-    
+    endGame() {
+        let x = this.snakeBox[this.snakeBox.length - 1].x;
+        let y = this.snakeBox[this.snakeBox.length - 1].y;
+
+        if (x > w - 1 || x < 0 || y > h - 1 || h < 0) {
+            return true;
+        }
+
+        for (let i = 0; i < this.snakeBox.length - 1; i++) {
+            let snakeBody = this.snakeBox[i];
+            if (snakeBody.x == x && snakeBody.y == y) {
+                return true;
+            }
+
+        }
+    }
+
 }
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,44 +1,58 @@
-let snake ;
+let snake;
 let w;
 let h;
 
+
+
 function setup() {
-    createCanvas(400, 400);
-    w = floor(width / resolution);
-    h = floor(height / resolution);
-    frameRate(5);
-    snake = new Snake();
-    food = createVector();
-   
-    
-  }
-  
-  function draw() {
-      scale(resolution);
-     background(51);
-     if(snake.eat(food)){
-         snake.foodLocation();
-     }
-     snake.update();
-     snake.show();
+  var snakeCanvas = createCanvas(400, 400);
+  snakeCanvas.parent("canvas");
+  w = floor(width / resolution);
+  h = floor(height / resolution);
+  frameRate(5);
+  snake = new Snake();
+  snake.foodLocation();
 
-     fill(255,0,0);
-     rect(food.x,food.y,1,1);
-    
-     
-    
+
+}
+
+function draw() {
+  scale(resolution);
+  background(51);
+  if (snake.eat(food)) {
+    snake.foodLocation();
   }
 
-  function keyPressed(){
+  snake.update();
+  snake.show();
 
-    if(keyCode === LEFT_ARROW ){
-      snake.setDir(-1,0);  
-    }else if(keyCode === RIGHT_ARROW){
-        snake.setDir(1,0); 
-    }else if(keyCode === UP_ARROW){
-        snake.setDir(0,-1); 
-    }else if(keyCode === DOWN_ARROW){
-        snake.setDir(0,1); 
-    }
-
+  if (snake.endGame()) {
+    textSize(1.7);
+    textFont('Serif');
+    text('End Game', 6, 10);
+    fill(255);
+    textSize(1);
+    text('Your score:' + " " + length, 6.5, 11.5);
+    noLoop();
   }
+  fill(255, 0, 0);
+  noStroke();
+  rect(food.x, food.y, 1, 1);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  }
+
+}
+function restart() {
+  window.location.reload();
+}
